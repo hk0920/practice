f331ee7bef53d20d613bc4561fdea8b8f4067b1a
HanHeeGue <gmlrb920@naver.com>
2023-04-17 20:41:43 +0900

    [add] react-day-picker library test

---
+++ react-day-picker/src/App.tsx
@@ -0,0 +1,36 @@
+import { addDays, format } from 'date-fns';
+import { useState } from 'react';
+import { DateRange, DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+
+const pastMonth = new Date(2023, 4, 17);
+
+function App() {
+  const [range, setRange] = useState<DateRange | undefined>();
+
+  console.log(range)
+
+  if (range?.from) {
+    
+    if (!range.to) {
+
+    } else if (range.to) {
+      // console.log(range.from > range.to)
+      
+    }
+  }
+
+  return (
+    <>
+      <DayPicker 
+        mode="range"
+        selected={range}
+        onSelect={setRange}
+        numberOfMonths={13} 
+        // showOutsideDays 
+      />;
+    </>
+  );
+}
+
+export default App;
